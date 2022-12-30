07345310fd6795a6da57c4a817018a84faa67072
Radwa Eid <radwaeid4@gmail.com>
2022-12-30 16:33:12 +0200

    fixed get teacher

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use App\Models\Subjectsassign;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,10 +19,21 @@ class TeacherController extends Controller
     {   
         $teacher = User::findOrFail($tcrId);
         $subs = Subjectsassign::all()->where('teacher_id',$teacher->id);
-        
+        $subjects_names =array();
+
+        foreach ($subs as $sub => $subject_id) {
+            $subjects = Subject::find($subject_id);
+            foreach ($subjects as $subject => $subject_name) {
+                
+                array_push($subjects_names,$subject_name->subject_name);
+            }
+            
+        }
+
+
+        $teacher['subjects'] =$subjects_names;
         return response()->json([
-            'teacher' => $teacher,
-            'subjects_assigned' => $subs,
+            'teacher' => $teacher
         ],200);
     }
 }
