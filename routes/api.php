6d261d43669b700506614eed46c85966446eb2d8
Radwa Eid <radwaeid4@gmail.com>
2023-01-07 00:42:05 +0200

    edited routes methods

--- routes/api.php
+++ routes/api.php
@@ -26,21 +26,21 @@ Route::middleware('auth:sanctum')->group(function(){
 
         //admin
         Route::get('getAdmins',[App\Http\Controllers\AdminController::class,'getAdmins']);
-        Route::get('getAdmin',[App\Http\Controllers\AdminController::class,'getAdmin']);
+        Route::post('getAdmin',[App\Http\Controllers\AdminController::class,'getAdmin']);
         Route::post('editAdmin',[App\Http\Controllers\AdminController::class,'editAdmin']);
         Route::post('deleteAdmin',[App\Http\Controllers\AdminController::class,'deleteAdmin']);
        
         
         //teacher
         Route::get('getTeachers',[App\Http\Controllers\TeacherController::class,'getTeachers']);
-        Route::get('getTeacher',[App\Http\Controllers\TeacherController::class,'getTeacher']);
+        Route::post('getTeacher',[App\Http\Controllers\TeacherController::class,'getTeacher']);
         Route::post('editTeacher',[App\Http\Controllers\AdminController::class,'editTeacher']);
         Route::post('deleteTeacher',[App\Http\Controllers\AdminController::class,'deleteTeacher']);
         
         //subject
         Route::post('createSubject',[App\Http\Controllers\SubjectController::class,'createSub']);
         Route::get('getSubjects',[App\Http\Controllers\SubjectController::class,'getSubjects']);
-        Route::get('getSubject',[App\Http\Controllers\SubjectController::class,'getSubject']);
+        Route::post('getSubject',[App\Http\Controllers\SubjectController::class,'getSubject']);
         Route::post('editSubject',[App\Http\Controllers\SubjectController::class,'editSubject']);
         Route::post('deleteSubject',[App\Http\Controllers\SubjectController::class,'deleteSubject']);
         Route::post('assignSub',[App\Http\Controllers\SubjectController::class,'assignSub']);
@@ -50,14 +50,14 @@ Route::middleware('auth:sanctum')->group(function(){
     });
     //CLOs
     Route::get('getClos',[App\Http\Controllers\CLOController::class,'getClos']);
-    Route::get('getClo',[App\Http\Controllers\CLOController::class,'getClo']);
-    Route::get('getSubClo',[App\Http\Controllers\CLOController::class,'getSubClo']);
+    Route::post('getClo',[App\Http\Controllers\CLOController::class,'getClo']);
+    Route::post('getSubClo',[App\Http\Controllers\CLOController::class,'getSubClo']);
     Route::post('createClo',[App\Http\Controllers\CLOController::class,'createClo']);
     Route::post('editClo',[App\Http\Controllers\CLOController::class,'editClo']);
     Route::post('deleteClo',[App\Http\Controllers\CLOController::class,'deleteClo']);
     
 
-    
+
     Route::post('/logout', [App\Http\Controllers\AuthController::class,'logout']);
     Route::get('/profile', [App\Http\Controllers\AuthController::class,'profile']);
     Route::post('/editProfile', [App\Http\Controllers\AuthController::class,'editProfile']);
